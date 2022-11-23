09aa051e292e243fb3ff280cfb671bc88a9ee772
Your Name <you@example.com>
2022-11-23 12:31:58 +0100

    8082

--- client/src/configuration.js
+++ client/src/configuration.js
@@ -1,7 +1,7 @@
 export const Configuration = { 
-	 FRONT_BASEURL: 'http://localhost:3000/',
-    BACK_BASEURL : 'http://localhost:4000/',  
-/*   FRONT_BASEURL: 'http://193.70.37.134/',
-    BACK_BASEURL : 'http://193.70.37.134:8080/',*/
+	/* FRONT_BASEURL: 'http://localhost:3000/',
+    BACK_BASEURL : 'http://localhost:4000/',  */
+   FRONT_BASEURL: 'http://54.36.183.243:82/',
+    BACK_BASEURL : 'http://54.36.183.243:8082/',
  };
 export default Configuration;
